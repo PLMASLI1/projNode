afa6dcf459cb0a131408e67d1720a2e1fbf1a88a
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2020-05-16 09:00:33 +0200

    Full CRUD on persons

--- public/js/app.js
+++ public/js/app.js
@@ -46,38 +46,66 @@ app.controller("Ctrl1", ["$scope", "$http", "$uibModal", function($scope, $http,
             function(rep) {
                 var options = { data: rep.data };
                 // open modal window with person data
-                var modalInstance = $uibModal.open({
-                    animation: true,
-                    ariaLabelledBy: 'modal-title-top',
-                    ariaDescribedBy: 'modal-body-top',
-                    templateUrl: 'editPersonDialog.html',
-                    controller: 'EditPersonCtrl',
-                    controllerAs: 'ctrl',
-                    resolve: {
-                        editPersonOptions: function () {
-                            return options;
-                        }
-                    }
-                });
-        
-                modalInstance.result.then(
-                    function (ret) {
-                        if(ret == 'delete') {
-                            console.log('delete ' + options.data._id);
-                        } else if(ret == 'save') {
-                            if(options.data._id) {
-                                console.log('update ', options.data);
-                            } else {
-                                console.log('insert ', options.data);
-                            }
-                        }
-                    },
-                    function (ret) {}
-                );
-        
+                editPerson(options);
             },
             function() {}
         );
     };
 
+    ctrl.newPerson = function() {
+        var options = { data: { firstName: '', lastName: '', email: '', yearofbirth: 1990 }, noDelete: true };
+        // open modal window with empty data
+        editPerson(options);
+    }
+
+    var editPerson = function(options) {
+
+        var modalInstance = $uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title-top',
+            ariaDescribedBy: 'modal-body-top',
+            templateUrl: 'editPersonDialog.html',
+            controller: 'EditPersonCtrl',
+            controllerAs: 'ctrl',
+            resolve: {
+                editPersonOptions: function () {
+                    return options;
+                }
+            }
+        });
+
+        modalInstance.result.then(
+            function (ret) {
+                if(ret == 'delete') {
+                    $http.delete('/person?_id='+options.data._id).then(
+                        function(ret) {
+                            ctrl.loadPersons();
+                        },
+                        function(err) {}
+                    );
+                } else if(ret == 'save') {
+                    if(options.data._id) {
+                        $http.put('/person', options.data).then(
+                            function(ret) {
+                                var n = ctrl.persons.findIndex(function(el) { 
+                                    return el._id == ret.data._id 
+                                }); // find the first element with specified _id
+                                ctrl.persons[n] = ret.data;
+                            },
+                            function(err) {}
+                        );
+                    } else {
+                        $http.post('/person', options.data).then(
+                            function(ret) {
+                                ctrl.loadPersons();
+                            },
+                            function(err) {}
+                        );
+                    }
+                }
+            },
+            function (ret) {}
+        );
+
+}
 }]);
